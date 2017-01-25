b1dcd4e13fc7ce1b0d5c3abc00bc2378101a20ea
Hajime Hoshi <hajimehoshi@gmail.com>
2017-01-25 12:33:31 +0900

    Enable to omit window title

--- GetWindowID.m
+++ GetWindowID.m
@@ -3,9 +3,23 @@
 
 int main(int argc, char **argv)
 {
-	NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements,kCGNullWindowID);
-	for(NSDictionary *window in windows)
-		if ([[window objectForKey:(NSString *)kCGWindowOwnerName] isEqualToString:[NSString stringWithUTF8String:argv[1]]])
-			if ([[window objectForKey:(NSString *)kCGWindowName] isEqualToString:[NSString stringWithUTF8String:argv[2]]])
-				printf("%d\n", [[window objectForKey:(NSString *)kCGWindowNumber] intValue]);
+  NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements,kCGNullWindowID);
+  for (NSDictionary *window in windows) {
+    NSString* ownerName = [window objectForKey:(NSString *)kCGWindowOwnerName];
+    if (![ownerName isEqualToString:[NSString stringWithUTF8String:argv[1]]]) {
+      continue;
+    }
+    NSString* windowName = [window objectForKey:(NSString *)kCGWindowName];
+    if (windowName == nil) {
+      continue;
+    }
+    if (argc > 2) {
+      if (![windowName isEqualToString:[NSString stringWithUTF8String:argv[2]]]) {
+        continue;
+      }
+    }
+    printf("%d\n", [[window objectForKey:(NSString *)kCGWindowNumber] intValue]);
+    break;
+  }
+  return 0;
 }
